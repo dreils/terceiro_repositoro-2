0bd1740e5091e35cb68416c7f38b4fd15d11233e
dreils <141249612+dreils@users.noreply.github.com>
2025-06-30 08:23:34 -0300

    aula 30/06

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function diminuiTamanho() {
         tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho() {
@@ -19,5 +20,44 @@ function aumentaTamanho() {
         tamanhoSenha = tamanhoSenha + 1 ;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox'); 
+
+
+const letraMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letraMinuscula = 'abcdefghijlmnopqrstuvwxyz';
+const numeros = '0123456789'
+const simbolos = '!@$%*?'
+
+geraSenha();
+
+function geraSenha () {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letraMaiuscula;
+    }
+
+     if (checkbox[1].checked) {
+        alfabeto = alfabeto + letraMinuscula;
+    }
+
+     if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+
+
+    let senha = '';
+    for(let i=0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto [numeroAleatorio];
+    }
+    campoSenha.value = senha;    
+}
